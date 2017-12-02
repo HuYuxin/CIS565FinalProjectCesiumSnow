7a47cfb647acd0b039dcafa4eb7a1272a9c62f41
Yuxin Hu <huyuxin1992@gmail.com>
2017-12-02 11:36:58 -0500

    added normal map to snow material

--- Source/Shaders/Materials/SnowMaterial.glsl
+++ Source/Shaders/Materials/SnowMaterial.glsl
@@ -1,16 +1,26 @@
-uniform sampler2D image;
+uniform sampler2D normalMap;
+uniform sampler2D whiteNoise;
 
 czm_material czm_getMaterial(czm_materialInput materialInput)
 {
     czm_material material = czm_getDefaultMaterial(materialInput);
-	
+
 	//Added by Yuxin For Test
-	//vec3 normal = normalize(materialInput.normalEC);
 	float snowSlope = materialInput.slope;
-	material.alpha = 1.0;
-	material.diffuse = vec3(1.0, 1.0, 1.0);
-	//material.diffuse = materialInput.normalEC;
-    //material.alpha = 1.0;
-	
+    material.alpha = snowSlope;
+	material.diffuse = vec3(0.8, 0.8, 0.9);
+    vec3 normalEC = materialInput.normalEC;
+    material.shininess = 200.0;
+
+    vec3 normalMapNormal = texture2D(normalMap, materialInput.st).rgb;
+    mat3 tangentToEye = materialInput.tangentToEyeMatrix;
+    material.normal  = tangentToEye * normalMapNormal;
+    material.specular = 0.9;
+
     return material;
 }
+
+
+
+
+
